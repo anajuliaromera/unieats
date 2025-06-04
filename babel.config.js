55dda81493e7b5dec17bc228703c19028b106e0e
Ana <anajuliapromera@gmail.com>
2025-06-04 00:37:14 -0300

    Ajustes finais no build, testes e configuração de ambiente

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }], // Essencial para Jest
-    '@babel/preset-typescript',                               // Se você usa TypeScript
-    ['@babel/preset-react', { runtime: 'automatic' }]      // Para transformar JSX
-  ],
-};
